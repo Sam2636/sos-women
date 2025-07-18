867d9fa8ea6d63da942a0a711cc6140acd87cb77
sam2636 <samdhanaseelan2636@gmail.com>
2025-07-18 22:22:12 +0530

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem')),
-    },
-    port: 5173,
-    host: true // optional: allows access from network devices
-  },
+  base: '/sos-women/',  // 👈 your GitHub repo name
 });
